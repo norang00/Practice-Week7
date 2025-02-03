1a3ea2126a4fbedd73661b5102e6f150b50e9d91
norang <norang1221@gmail.com>
2025-02-03 09:31:21 +0900

    Initial Commit

---
+++ Week7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week7
+//
+//  Created by Kyuhee hong on 2/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
